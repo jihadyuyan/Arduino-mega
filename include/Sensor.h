696418202ba830486a017b4310af147655b787e4
jihadyuyan <jihadyuyan97@gmail.com>
2023-11-15 14:12:23 +0700

    initiate

---
+++ include/Sensor.h
@@ -0,0 +1,18 @@
+#ifndef Sensor_H_
+#define Sensor_H_
+
+#include <MQUnifiedsensor.h>
+
+class Sensor
+{
+public:
+    Sensor();
+    void init_MQ4();
+    void init_MQ135();
+    void init_MQ7();
+    float get_MQ4();
+    float get_MQ135();
+    float get_MQ7();
+};
+
+#endif // !Sensor_H_
