51c678dd0e58804b8ff612fc0afc6bba7d9f03e3
frn-af <1911512017_farhan@student.unand.ac.id>
2023-12-04 20:04:21 +0700

    Add MQ7 sensor initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Sensor.h>
 #include <FuzzyLogic.h>
+#include <Wire.h>
 
 FuzzyLogic *fuzzyLogic;
 Sensor *sensor;
@@ -20,7 +21,7 @@ void loop()
 
   float nh3 = sensor->get_MQ135();
   float ch4 = sensor->get_MQ4();
-  float co = 0;
+  float co = sensor->get_MQ7();
   Serial.print("MQ135: ");
   Serial.println(nh3);
   Serial.print("MQ4: ");
@@ -28,6 +29,7 @@ void loop()
   Serial.print("MQ7: ");
   Serial.println(co);
   fuzzyLogic->fuzzy_compute(nh3, ch4, co);
+
   delay(5000);
 }
 void sensor_init()
@@ -35,6 +37,7 @@ void sensor_init()
   sensor = new Sensor();
   sensor->init_MQ4();
   sensor->init_MQ135();
+  sensor->init_MQ7();
 }
 
 void fuzzy_logic_init()
