cd759e551eb527a889987bca4e466d8047cae89f
frn-af <1911512017_farhan@student.unand.ac.id>
2023-12-10 15:46:23 +0700

    Refactor show_lcd function to accept a String parameter

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,6 @@
 #include "Adafruit_GFX.h"
 #include "MCUFRIEND_kbv.h"
 
-#include "Fonts/FreeSans9pt7b.h"
-
 #define LCD_CS 2
 #define LCD_RS 32
 #define LCD_WR 33
@@ -21,7 +19,7 @@ Sensor *sensor;
 
 void sensor_init();
 void fuzzy_logic_init();
-void show_lcd(int x, int y, String sz, const GFXfont *f, const char *message);
+void show_lcd(int x, int y, int sz, const GFXfont *f, String message);
 
 void setup()
 {
@@ -49,17 +47,9 @@ void loop()
   Serial.print("MQ7: ");
   Serial.println(co);
   float output = fuzzyLogic->fuzzy_compute(nh3, ch4, co);
+  show_lcd(100, 200, 1, &FreeSans9pt7b, "NH3: " + String(nh3)); // Convert nh3 to a string
 
   delay(5000);
-}
-
-void show_lcd(int x, int y, int sz, const GFXfont *f, const char *message)
-{
-  tft.setFont(f);
-  tft.setCursor(x, y);
-  tft.setTextColor(TFT_BLACK);
-  tft.setTextSize(sz);
-  tft.println(message);
 }
 
 void sensor_init()
@@ -74,4 +64,13 @@ void fuzzy_logic_init()
 {
   fuzzyLogic = new FuzzyLogic();
   fuzzyLogic->fuzzy_init();
+}
+
+void show_lcd(int x, int y, int sz, const GFXfont *f, String message)
+{
+  tft.setFont(f);
+  tft.setCursor(x, y);
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(sz);
+  tft.println(message);
 }
